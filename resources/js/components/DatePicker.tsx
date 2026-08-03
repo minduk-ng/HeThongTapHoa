2fb46549fd5a9c58f1e6245fd69d847ca43b7e95
minduk <ngminhduc05.ds@gmail.com>
2026-08-03 11:44:49 +0700

    fix: nang z-index popup DatePicker len tren overlay form drawer

--- resources/js/components/DatePicker.tsx
+++ resources/js/components/DatePicker.tsx
@@ -612,7 +612,7 @@ export default function DatePicker(props: DatePickerProps) {
                     <div
                         ref={panelRef}
                         style={{ top: panelPos.top, left: panelPos.left }}
-                        className="fixed z-50 w-[300px] rounded-2xl border border-zinc-200/80 bg-white p-3 shadow-xl dark:border-zinc-800/80 dark:bg-zinc-900"
+                        className="fixed z-[110] w-[300px] rounded-2xl border border-zinc-200/80 bg-white p-3 shadow-xl dark:border-zinc-800/80 dark:bg-zinc-900"
                     >
                         <div className="flex items-center justify-between px-1 pb-2">
                             <button
