fb797bbe9341947f9c7740dac5ba268f679c0317
minduk <ngminhduc05.ds@gmail.com>
2026-08-13 14:48:41 +0700

    feat: bang promotion_time_slots + model + relation timeSlots

---
+++ app/Models/PromotionTimeSlot.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class PromotionTimeSlot extends Model
+{
+    protected $fillable = ['promotion_id', 'day_of_week', 'start_time', 'end_time'];
+
+    protected $casts = [
+        'day_of_week' => 'int',
+    ];
+
+    public function promotion(): BelongsTo
+    {
+        return $this->belongsTo(Promotion::class);
+    }
+}
